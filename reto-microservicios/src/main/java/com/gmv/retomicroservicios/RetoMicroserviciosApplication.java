cac714f61b917bffaa213fc278e49a0942eb2b70
Jose Antonio Torrecilla Fuentes <jatf2@yahoo.es>
2017-10-20 16:40:45 +0200

    Creando proyecto Spring Boot con Maven

---
+++ reto-microservicios/src/main/java/com/gmv/retomicroservicios/RetoMicroserviciosApplication.java
@@ -0,0 +1,12 @@
+package com.gmv.retomicroservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoMicroserviciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoMicroserviciosApplication.class, args);
+	}
+}
